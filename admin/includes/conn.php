0632a11a348834039a9410fc0b68c7b3303701df
Fiattor-Lorlornyo-John <dynamicjnr123@gmail.com>
2024-09-01 16:54:15 +0000

    eighth commit

--- admin/includes/conn.php
+++ admin/includes/conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('http://sql5.freesqldatabase.com/', 'thepapisogram', 'Tony2004.', 'chatterly');
+	$conn = new mysqli('localhost', 'root', '', 'votesystem');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
